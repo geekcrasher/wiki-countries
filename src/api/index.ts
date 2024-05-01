0356d24f7e6342728f8a922a71910ea39f2840e0
Paulo Lopez <geekcrasher@gmail.com>
2024-05-01 21:08:35 +0800

    feat: created a function to retrieve country data using restcountries api

---
+++ src/api/index.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosError } from "axios";
+
+
+// @function for retrieving the data for all countries
+export const fetchAllCountriesData = async () => {
+  try {
+    const response = await axios.get("https://restcountries.com/v3.1/all", {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    })
+    return response?.data
+  } catch (error: unknown) {
+
+    if (error instanceof AxiosError) {
+      console.log(`Error fetching data: `, error.message)
+      throw error
+    }
+  }
+}
+
+// @ function for retrieving the data for a specific country.
+export const fetchSingleCountryData = async (countryName: string) => {
+  try {
+    const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}`, {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    })
+    return response?.data
+  } catch (error: unknown) {
+
+    if (error instanceof AxiosError) {
+      console.log(`Error fetching data for "${countryName}":`, error.message)
+      throw error
+    }
+  }
+}
