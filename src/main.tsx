d6b8137d36d880c18341c013a419dda0bade0063
Paulo Lopez <geekcrasher@gmail.com>
2024-05-08 18:53:50 +0800

    feat: created a route collections and wrapped inside the ThemeContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { CountriesContextProvider } from './context/CountriesContext.tsx'
+import { ThemeContextProvider } from './context/ThemeContext.tsx'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Home from './components/Home/index.tsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index path='/' element={<Home />} />
+      <Route path='country/:name' element={<></>} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CountriesContextProvider>
-      <App />
-    </CountriesContextProvider>
+    <ThemeContextProvider>
+      <RouterProvider router={router} />
+    </ThemeContextProvider>
   </React.StrictMode>,
 )
