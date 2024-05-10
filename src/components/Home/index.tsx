e6dd9e3ff551a69dcc83e550e54b33b90b9d865a
Paulo Lopez <geekcrasher@gmail.com>
2024-05-10 23:03:43 +0800

    refactor: removed the CountriesContextProvider

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,15 +1,12 @@
-import Navbar from "@/components/Navbar";
-import { CountriesContextProvider } from "@/context/CountriesContext";
-import Content from "../Content";
+import Content from "@/components/Content";
+import SearchBar from "@/components/Searchbar";
 
 const Home = () => {
   return (
-    <CountriesContextProvider>
-      <section className="flex flex-col relative">
-        <Navbar />
-        <Content />
-      </section>
-    </CountriesContextProvider>
+    <section className="flex flex-col relative mt-20 py-16 px-20  border min-h-[calc(100vh-5.5rem)]">
+      <SearchBar />
+      <Content />
+    </section>
   );
 }
 
