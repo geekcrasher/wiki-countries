b557db9546753526a196da43d0432f7f50f8a39d
Paulo Lopez <geekcrasher@gmail.com>
2024-05-13 20:44:41 +0800

    created the country;s types

---
+++ src/lib/types.ts
@@ -0,0 +1,31 @@
+
+type Name = {
+  common: string,
+  official: string
+}
+
+type Currency = {
+  name: string,
+  symbol: string
+}
+
+type Flag = {
+  svg: string,
+  alt: string
+}
+
+
+export type Country = {
+  name: Name,
+  population: number,
+  region: string,
+  capital: string[],
+  subregion: string,
+  maps: { googleMaps: string }
+  continents: string[],
+  languages: { [key: string]: string }
+  currencies: { [key: string]: Currency }
+  flags: Flag
+  coatOfArms: { svg: string }
+  tld: string[]
+}
