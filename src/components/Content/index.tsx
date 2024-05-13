75883dc41a89332aa4ffa8bfd5343d1aac6abf98
Paulo Lopez <geekcrasher@gmail.com>
2024-05-13 20:55:23 +0800

    feat: created a search filter functionality and imported the Link component from react-router to navigate in cOuntry page

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -9,38 +9,41 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { url } from "inspector";
+import { Link } from "react-router-dom";
+import { formattedPopulation } from "@/lib/formattedPopulation";
+import { Paragraph } from "@/components/shared/Paragraph";
+
 
 const Content = React.memo(() => {
 
-  const { countries } = useCountries()
+  const { countries, countrySearch } = useCountries()
 
-  const data = countries?.find(country => country.name.common === 'Germany')
-
-  console.log(data)
+  const filteredCountry = countries?.filter(country => {
+    return country.name.common.toLowerCase().includes(countrySearch.toLowerCase())
+  })
 
   return (
     <>
-      <section className="grid 2xl:grid-cols-4 gap-14 mt-14">
-        {countries?.map(country => {
+      <section className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10 mt-14">
+        {filteredCountry?.map(country => {
           return (
-            <section key={country.name.common} className="shadow-md">
-              <Card className="h-[22rem] p-0 border-0 overflow-hidden">
+            <Link to={`/country/${country.name.common}`} key={country.name.common} className="shadow-md">
+              <Card className="h-[20rem] md:h-[22rem] p-0 border-0 overflow-hidden">
                 <CardHeader
-                  className="h-[12rem] p-0 bg-contain bg-no-repeat bg-center "
+                  className="h-[10rem] md:h-[12rem] p-0 bg-contain bg-no-repeat bg-center "
                   style={{ backgroundImage: `url(${country.flags.svg})` }}
                 >
                 </CardHeader>
                 <CardContent className="mt-4">
                   <h2 className="text-xl mb-2 font-medium">{country.name.common}</h2>
-                  <div>
-                    <p className="font-medium">Population: <span className="font-normal">{country.population}</span></p>
-                    <p className="font-medium">Region: <span className="font-normal">{country.region}</span></p>
-                    <p className="font-medium">Capital: <span className="font-normal">{country.capital}</span></p>
-                  </div>
+                  <section>
+                    <Paragraph label="Population" countryInfo={formattedPopulation(country.population)} />
+                    <Paragraph label="Region" countryInfo={country.region} />
+                    <Paragraph label="Capital" countryInfo={country.capital} />
+                  </section>
                 </CardContent>
               </Card>
-            </section>
+            </Link>
           )
         })}
       </section>
