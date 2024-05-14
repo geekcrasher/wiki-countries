7afd408cf42bf4c21d057896d5d4aa9814b33f91
Paulo Lopez <geekcrasher@gmail.com>
2024-05-14 22:09:10 +0800

    created the region array for riltering the country region/continent

---
+++ src/components/Searchbar/region.ts
@@ -0,0 +1,24 @@
+type RegionType = {
+  readonly label: string,
+  readonly value: string
+}
+
+export const regions: RegionType[] = [
+  {
+    label: 'Africa',
+    value: 'africa'
+  },
+  {
+    label: 'America',
+    value: 'america'
+  }, {
+    label: 'Asia',
+    value: 'asia'
+  }, {
+    label: 'Europe',
+    value: 'europe'
+  }, {
+    label: 'Oceania',
+    value: 'oceania'
+  },
+]
