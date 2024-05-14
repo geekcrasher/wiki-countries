9a9496f20aebcbd9f1bbde121ece310288756b06
Paulo Lopez <geekcrasher@gmail.com>
2024-05-14 22:23:12 +0800

    style: added the default text-color of the paragraph tag

--- src/components/shared/Paragraph.tsx
+++ src/components/shared/Paragraph.tsx
@@ -7,7 +7,7 @@ type ParagraphType<T> = {
 export const Paragraph = <T extends React.ReactNode>({ label, countryInfo }: ParagraphType<T>) => {
   return (
     <p className="font-medium">
-      {label}: <span className="font-normal"> {countryInfo}</span>
+      {label}: <span className="font-normal dark:text-gray-400"> {countryInfo ?? 'NA'}</span>
     </p>
   )
 }
