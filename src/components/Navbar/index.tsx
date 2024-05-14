909b27bee79b9bfac1050d572174b9245ccc97c8
Paulo Lopez <geekcrasher@gmail.com>
2024-05-14 22:22:26 +0800

    style: changed the bg color of the navbar component in darkmode

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -16,8 +16,8 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="fixed z-10 flex items-center justify-between px-6 h-[5.5rem] w-full shadow-sm bg-white">
-        <Button onClick={navigateToHomePage} className="flex items-center gap-2 bg-transparent text-base text-[#333] sm:text-lg font-medium">
+      <nav className="fixed z-10 flex items-center justify-between px-2 sm:px-6 h-[5.5rem] w-full shadow-sm bg-white dark:bg-[#2b3945]">
+        <Button onClick={navigateToHomePage} className="flex items-center gap-4 bg-transparent text-base text-[#333] dark:text-slate-50 sm:text-lg font-medium">
           <span><Earth color="#f56565" size={24} /></span>
           Wiki Countries
         </Button>
