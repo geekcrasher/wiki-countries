f384c946e693cc3944549ddda9c003671a7aa417
Paulo Lopez <geekcrasher@gmail.com>
2024-05-14 22:13:12 +0800

    style: added a bagcground color for darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar"
 function App() {
 
   return (
-    <main className='min-h-screen w-full flex flex-col bg-gray-50'>
+    <main className='min-h-screen w-full flex flex-col bg-gray-50 dark:bg-[#202c37]'>
       <Navbar />
       <Outlet />
     </main>
