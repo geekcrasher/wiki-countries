2718bf7d01997845aba71f58ab3852470cc2fe5c
Paulo Lopez <geekcrasher@gmail.com>
2024-05-14 22:26:10 +0800

    refactor: changed the previous tailwind properties:

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,15 +1,11 @@
-import { useCountries } from "@/context/CountriesContext";
 import React from "react";
-import SearchBar from "../Searchbar";
+import { useCountries } from "@/context/CountriesContext";
+import { Link } from "react-router-dom";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card"
-import { Link } from "react-router-dom";
 import { formattedPopulation } from "@/lib/formattedPopulation";
 import { Paragraph } from "@/components/shared/Paragraph";
 
@@ -34,9 +30,9 @@ const Content = React.memo(() => {
                   style={{ backgroundImage: `url(${country.flags.svg})` }}
                 >
                 </CardHeader>
-                <CardContent className="mt-4">
+                <CardContent className="mt-2">
                   <h2 className="text-xl mb-2 font-medium">{country.name.common}</h2>
-                  <section>
+                  <section className="text-sm space-y-1 mt-4">
                     <Paragraph label="Population" countryInfo={formattedPopulation(country.population)} />
                     <Paragraph label="Region" countryInfo={country.region} />
                     <Paragraph label="Capital" countryInfo={country.capital} />
