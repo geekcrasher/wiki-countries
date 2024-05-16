03cec0e72c277fafb464821d0eba78dc23f3f48f
Paulo Lopez <geekcrasher@gmail.com>
2024-05-17 00:13:36 +0800

    perf: make the content component lazy and wrapped it inside the Suspense

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,18 @@
-import Content from "@/components/Content";
+import { lazy, Suspense } from "react";
 import SearchBar from "@/components/Searchbar";
+import CountriesSkeleton from "@/components/Skeleton/CountriesSkeleton";
+
+const Content = lazy(() => import("@/components/Content"))
 
 const Home = () => {
   return (
     <section className="flex flex-col relative mt-20 py-16 px-10 md:px-16 lg:px-20 min-h-[calc(100vh-5.5rem)]">
       <SearchBar />
-      <Content />
+      <section className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10 mt-14">
+        <Suspense fallback={<CountriesSkeleton />}>
+          <Content />
+        </Suspense>
+      </section>
     </section>
   );
 }
