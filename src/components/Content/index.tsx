d3d2fdb25a5c947744e26b5455581b44a5a28e5b
Paulo Lopez <geekcrasher@gmail.com>
2024-05-17 00:12:57 +0800

    refactor: added the filtered countries based on the selected region

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -12,37 +12,42 @@ import { Paragraph } from "@/components/shared/Paragraph";
 
 const Content = React.memo(() => {
 
-  const { countries, countrySearch } = useCountries()
+  const { countries, countrySearch, region } = useCountries()
 
   const filteredCountry = countries?.filter(country => {
-    return country.name.common.toLowerCase().includes(countrySearch.toLowerCase())
+    const searchCountry = country.name.common.toLowerCase().includes(countrySearch.toLowerCase());
+    const searchRegion = country.region.toLowerCase() === region
+
+    if (region === 'all') {
+      return country
+    }
+
+    return searchCountry && searchRegion
   })
 
   return (
     <>
-      <section className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10 mt-14">
-        {filteredCountry?.map(country => {
-          return (
-            <Link to={`/country/${country.name.common}`} key={country.name.common} className="shadow-md">
-              <Card className="h-[20rem] md:h-[22rem] p-0 border-0 overflow-hidden">
-                <CardHeader
-                  className="h-[10rem] md:h-[12rem] p-0 bg-contain bg-no-repeat bg-center "
-                  style={{ backgroundImage: `url(${country.flags.svg})` }}
-                >
-                </CardHeader>
-                <CardContent className="mt-2">
-                  <h2 className="text-xl mb-2 font-medium">{country.name.common}</h2>
-                  <section className="text-sm space-y-1 mt-4">
-                    <Paragraph label="Population" countryInfo={formattedPopulation(country.population)} />
-                    <Paragraph label="Region" countryInfo={country.region} />
-                    <Paragraph label="Capital" countryInfo={country.capital} />
-                  </section>
-                </CardContent>
-              </Card>
-            </Link>
-          )
-        })}
-      </section>
+      {filteredCountry?.map(country => {
+        return (
+          <Link to={`/country/${country.name.common}`} key={country.name.common} className="shadow-md overflow-hidden rounded-lg">
+            <Card className="h-[20rem] md:h-[22rem] p-0 border-0 overflow-hidden">
+              <CardHeader
+                className="h-[10rem] md:h-[12rem] p-0 bg-contain bg-no-repeat bg-center "
+                style={{ backgroundImage: `url(${country.flags.svg})` }}
+              >
+              </CardHeader>
+              <CardContent className="mt-2">
+                <h2 className="text-xl mb-2 font-medium">{country.name.common}</h2>
+                <section className="text-sm space-y-1 mt-4">
+                  <Paragraph label="Population" countryInfo={formattedPopulation(country.population)} />
+                  <Paragraph label="Region" countryInfo={country.region} />
+                  <Paragraph label="Capital" countryInfo={country.capital} />
+                </section>
+              </CardContent>
+            </Card>
+          </Link>
+        )
+      })}
     </>
   );
 })
