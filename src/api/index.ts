75f2d28f524409eabfbe731ed35de823dda77d3d
Paulo Lopez <geekcrasher@gmail.com>
2024-05-24 17:22:42 +0800

    refactor: removed the function for fetching country data. Declared the return type of the fethAllCountriesData function

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosError } from "axios";
+import { type Country } from "@/lib/types";
 
 
 // @function for retrieving the data for all countries
-export const fetchAllCountriesData = async () => {
+export const fetchAllCountriesData = async (): Promise<Country[]> => {
   try {
-    const response = await axios.get("https://restcountries.com/v3.1/all", {
+    const response = await axios.get<Country[]>("https://restcountries.com/v3.1/all", {
       headers: {
         "Content-Type": "application/json; charset=utf-8"
       }
@@ -15,24 +16,9 @@ export const fetchAllCountriesData = async () => {
     if (error instanceof AxiosError) {
       console.log(`Error fetching data: `, error.message)
       throw error
-    }
-  }
-}
-
-// @ function for retrieving the data for a specific country.
-export const fetchSingleCountryData = async (countryName: string) => {
-  try {
-    const response = await axios.get(`https://restcountries.com/v3.1/name/${countryName}`, {
-      headers: {
-        "Content-Type": "application/json; charset=utf-8"
-      }
-    })
-    return response?.data
-  } catch (error: unknown) {
-
-    if (error instanceof AxiosError) {
-      console.log(`Error fetching data for "${countryName}":`, error.message)
-      throw error
+    } else {
+      console.error(`Unexpected error while fetching the data":`, error);
+      throw new Error(`Unexpected error: ${error}`);
     }
   }
 }
@@ -51,6 +37,9 @@ export const fetchCountryCode = async (borderName?: string[]) => {
     if (error instanceof AxiosError) {
       console.log(`Error fetching data for "${borderName}":`, error.message)
       throw error
+    } else {
+      console.error(`Unexpected error while fetching data for "${borderName}":`, error);
+      throw new Error(`Unexpected error: ${error}`);
     }
   }
 }
