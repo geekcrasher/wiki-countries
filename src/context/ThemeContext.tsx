1fab32d6734a21e7e2c4094dc92461334b853e67
Paulo Lopez <geekcrasher@gmail.com>
2024-05-24 17:26:28 +0800

    refactor: store the theme in localstorage

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -3,37 +3,41 @@ import { createContext, useContext, useEffect, useState } from "react"
 type Theme = 'light' | 'dark'
 
 type ThemeContextType = {
-  darkMode: boolean,
   theme: Theme,
   handleChangeTheme: () => void
 }
 
 const ThemeContext = createContext<ThemeContextType | null>(null)
 
-export const useTheme = () => {
-  return useContext(ThemeContext) as ThemeContextType
+export const useTheme = (): ThemeContextType => {
+  const context = useContext(ThemeContext)
+  if (!context) {
+    throw new Error('useCountries must be used within a CountriesProvider')
+  }
+  return context
 }
 
 export const ThemeContextProvider = ({ children }: { children: React.ReactNode }) => {
-
-  const [darkMode, setDarkMode] = useState(false)
   const [theme, setTheme] = useState<Theme>('light')
 
   useEffect(() => {
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark")
+    const storedTheme = localStorage.getItem('theme') as Theme;
+    if (storedTheme && storedTheme === 'dark') {
+      setTheme(storedTheme);
+      document.documentElement.classList.add(storedTheme);
     } else {
-      document.documentElement.classList.remove("dark")
+      document.documentElement.classList.remove('dark');
     }
   }, [theme])
 
   const handleChangeTheme = (): void => {
-    setDarkMode(!darkMode)
-    setTheme(theme === "dark" ? "light" : "dark")
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
   }
 
   return (
-    <ThemeContext.Provider value={{ darkMode, theme, handleChangeTheme }}>
+    <ThemeContext.Provider value={{ theme, handleChangeTheme }}>
       {children}
     </ThemeContext.Provider>
   )
